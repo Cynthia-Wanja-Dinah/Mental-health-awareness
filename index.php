30311be29153cb9a045dd57b448155cee62b0f3f
Cynthia-Wanja-Dinah <cynthiawanja618@gmail.com>
2024-05-10 20:25:26 +0300

    mentalhealth

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mental awareness</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+
+    
+    <nav>
+        <div class="logo">
+            <img src="images/amani.png" alt="Logo">
+        </div>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <a href="#menu"></a>
+        <a href="support.php"><button class="order-button"  style=" border-radius:30px; padding: 6px; width:100px; margin-bottom:10px;">Get Help</button>
+            </button></a>
+        <a href="donate.php"><button class="order-btn" style=" border-radius:30px; padding: 8px; border:none; width:100px; background-color:#4CAF50;color:white" >Donate</button>
+            </button></a>
+        
+    </nav>
+
+   <section id="about">
+
+
+
+
+   </section>
+
+
+
+
+
+
+
+
+    <section id="home">
+        <div class="container">
+        <h2 style="color:aliceblue; margin-left:20px; ">Amani Minds </h2>
+        <p style="font-weight: 20px;">Everyone Deserves a good mental Health</p>
+    </div>
+        <div class="learn-more">
+          <p style="font-size: 18px; margin-bottom:5px; margin-right:100px;  font-size:30px; font-weight:50px">Your mental Health Matters</p>
+            <a href="more.php"><button style="margin-left: 300px;  border-radius:30px;  margin-bottom:100px;">Get Help</button></a>
+          
+        </div>
+   
+          
+    </section>
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+<section id="contact">
+    <div class="contact-section">
+        <h2>Contact Us</h2>
+        <div class="contact-form">
+            <form action="contact.php" method="post">
+                <div class="form-group">
+                    <label for="name" style="color: black;">Name:</label>
+                    <input type="text" id="name" name="name" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="email" style="color: black;">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="message" style="color: black;">Message:</label>
+                    <textarea id="message" name="message" rows="5" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</section>
+
+
+
+
+<section id="footer">
+    <div class="footer">
+        <div class="footer-column">
+          <h3>About Us</h3>
+          <a href="#"> <i class="fa fa-facebook-official" aria-hidden="true"></i>Facebook</a>
+          <a href="#">Twitter</a>
+          <a href="#">Instagram</a>
+          <a href="#">LinkedIn</a>
+        </div>
+    
+        <div class="footer-column">
+          <h3>Quick Links</h3>
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+          <a href="#">Services</a>
+          <a href="#">FAQ</a>
+        </div>
+    
+        <div class="footer-column">
+          <h3>Contact Us</h3>
+          <div class="footer-icons">
+            <div class="icon">&#x1F4CD;</div> 
+            <a href="#">Nakuru, City</a>
+          </div>
+          <div class="footer-icons">
+            <div class="icon">&#x260E;</div> 
+            <a href="#">+254707395450</a>
+          </div>
+          <div class="footer-icons">
+            <div class="icon">&#9993;</div> 
+            <a href="mailto:info@example.com">empowerhealth@gmail.com</a>
+          </div>
+        </div>
+      </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+    </body>
+    </html>
